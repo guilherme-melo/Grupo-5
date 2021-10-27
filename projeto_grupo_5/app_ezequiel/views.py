438e5acc63be6584aa4ee334c738f8e1136c4455
EzequielEBS <ezequielsantos2306@gmail.com>
2021-10-27 19:04:15 -0300

    Corrigi alguns detalhes e adicionei template

--- projeto_grupo_5/app_ezequiel/views.py
+++ projeto_grupo_5/app_ezequiel/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("<strong>EZEQUIEL</strong>")
+    return render(request, "app_ezequiel/index.htm")
--- projeto_grupo_5/projeto_grupo_5/urls.py
+++ projeto_grupo_5/projeto_grupo_5/urls.py
@@ -4,7 +4,7 @@ from django.urls import path,include
 urlpatterns = [
     path(r"projeto_grupo_5/", include("projeto_app.urls")),
     path(r"projeto_grupo_5/breno/", include("breno.urls")),
-    path(r"Jacob/", include("Jacob.urls")), path('admin/', admin.site.urls),
+    path(r"Jacob/", include("Jacob.urls")),
     path(r"app_ezequiel/", include("app_ezequiel.urls")),
     path(r"app_wendell/", include("app_wendell.urls")),
     path(r"appdan/", include("appdan.urls")),
