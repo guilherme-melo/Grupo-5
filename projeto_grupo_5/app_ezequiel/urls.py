882d1cff85bff52a6a8f500eca57a28be5a31c20
EzequielEBS <ezequielsantos2306@gmail.com>
2021-10-29 09:04:34 -0300

    atualizei settings.py e templates

--- projeto_grupo_5/app_ezequiel/urls.py
+++ projeto_grupo_5/app_ezequiel/urls.py
@@ -4,4 +4,5 @@ from app_ezequiel import views as views
 urlpatterns = [
     path("", views.index, name="index"),
     path("index/", views.index, name="index"),
+    path("special/", views.special, name="special")
 ]
--- projeto_grupo_5/app_ezequiel/views.py
+++ projeto_grupo_5/app_ezequiel/views.py
@@ -2,4 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
+    return HttpResponse("<strong>Ezequiel</strong>")
+
+def special(request):
     return render(request, "app_ezequiel/index.htm")
