be73b6519fb8b06b8c14b894ce0394e679022106
EzequielEBS <ezequielsantos2306@gmail.com>
2021-11-08 09:06:03 -0300

    Corrigindo pequeno problema de url

--- projeto_grupo_5/projeto_grupo_5/urls.py
+++ projeto_grupo_5/projeto_grupo_5/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path(r"projeto_grupo_5/", include("projeto_app.urls")),
+    path(r"app_guilherme/", include("app_guilherme.urls")),
     path(r"projeto_grupo_5/breno/", include("breno.urls")),
     path(r"Jacob/", include("Jacob.urls")),
     path(r"app_ezequiel/", include("app_ezequiel.urls")),
