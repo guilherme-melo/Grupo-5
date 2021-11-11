dcb062ef0147adf1a8312f5c1fc3e30d5bdc3275
Daniel Jacob Tonn <danieljt.djt@gmail.com>
2021-11-11 20:58:01 -0300

    modifiquei uma caralhada de coisa

--- projeto_grupo_5/Jacob/urls.py
+++ projeto_grupo_5/Jacob/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path
-from Jacob import views as Jacob_views
+from Jacob import views as views
 
 urlpatterns = [
-    path("",Jacob_views.index, name="index"),
-    path("index/", Jacob_views.index, name ="index")
+    path("", views.primeiro, name="primeiro"),
+    path("primeiro/", views.primeiro, name="primeiro"),
+    path("primeiro/<str:pessoa>", views.equipe, name="equipe"),
+    path("redireciona/", views.redireciona, name="redireciona")
 ]
--- projeto_grupo_5/Jacob/views.py
+++ projeto_grupo_5/Jacob/views.py
@@ -1,5 +1,30 @@
-from django.http import HttpResponse
+from django.conf.urls import url
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
-def index(request):
-    return HttpResponse("<strong>JACOB</strong>")
+def primeiro(request):
+    return render(request, "Jacob/primeiro.htm")
+
+def equipe(request, pessoa):
+    equipe = ["dandan", "daniel", "guilherme", "breno", "wendell", "ezequiel"]
+    cd = ["guilherme", "breno"]
+    map = ["dandan", "wendell", "ezequiel", "daniel"]
+    if pessoa in equipe:
+        if pessoa in cd:
+            curso = "CIÊNCIAS DE DADOS"
+        elif pessoa in map:
+            curso = "MATEMÁTICA APLICADA"
+        context ={
+            "nome": "amiguinho",
+            "nome_familia": pessoa.capitalize(),
+            "curso": curso,
+            "equipe": equipe
+        }
+        return render(request, "Jacob/equipe.htm", context)
+    else:
+        return HttpResponseNotFound("Página não existe!")
+
+def redireciona(request):
+    url_redirecionamento = reverse("primeiro")
+    return HttpResponseRedirect(url_redirecionamento)
