236d714f7853de712b447778190b55104d79e7ca
Breno Azevedo <eborn.azevedo@gmail.com>
2021-11-11 20:57:56 -0300

    Atualizaçao views.py - Breno

--- projeto_grupo_5/breno/views.py
+++ projeto_grupo_5/breno/views.py
@@ -1,6 +1,14 @@
-from django.http.response import HttpResponse
+from django.conf.urls import url
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello, world!")
+def sobre(request):
+    return render(request, "app_breno/sobre.htm")
+
+def lista(request):
+    return render(request, "app_breno/lista.htm")
+
+def tabela(request):
+    return render(request, "app_breno/tabela.htm")
+    
